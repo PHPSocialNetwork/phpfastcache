ffa144e47549a17b981ea88523db59a9150ecb2e
Geolim4 <contact@geolim4.com>
2016-02-06 00:51:56 +0100

    Added travis syntax checker. Unit tests will comme later.

---
+++ .travis/checkSyntax.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
+ * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ */
+function read_dir($dir, $ext = null)
+{
+    $list = [];
+    $dir .= '/';
+    if (($res = opendir($dir)) === false) {
+        exit(1);
+    }
+    while (($name = readdir($res)) !== false) {
+        if ($name == '.' || $name == '..') {
+            continue;
+        }
+        $name = $dir . $name;
+        if (is_dir($name)) {
+            $list = array_merge($list, read_dir($name, $ext));
+        } elseif (is_file($name)) {
+            if (!is_null($ext) && substr(strrchr($name, '.'), 1) != $ext) {
+                continue;
+            }
+            $list[] = $name;
+        }
+    }
+
+    return $list;
+}
+
+$list = read_dir('.', 'php');
+$list += read_dir('.', 'tpl');
+
+$exit = 0;
+foreach ($list as $file) {
+    $output = '';
+
+    exec('php -lf ' . $file, $output, $status);
+    echo(implode("\n", $output) . "\n");
+    if ($status != 0) {
+        $exit = $status;
+    }
+}
+exit($exit);

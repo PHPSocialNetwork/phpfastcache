dcdb92d7836ba3aa46f97b956d005a45d74bf41f
Geolim4 <contact@geolim4.com>
2016-02-07 08:50:30 +0100

    Updated all drivers examples

---
+++ examples/index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo '<h2> Drivers available: </h2>';
+echo '<ul>';
+
+foreach (glob("*.php") as $filename) {
+	if(basename(__FILE__) !== $filename){
+		 echo "<li><a href=\"$filename\">" . ucfirst(basename($filename, '.php')) . "</a></li>";
+	}
+}
+
+echo '</ul>';
+
+// Comment this "exit;" to display extensions 
+// informations about missing/misconfigured drivers
+exit;
+
+echo '<h2> PHP extensions loaded:  </h2>';
+echo '<ul>';
+
+$extensions = array_map('ucfirst', get_loaded_extensions());
+sort($extensions);
+
+foreach ($extensions as $extension) {
+        echo "<li>$extension</li>";
+}
+
+echo '</ul>';
+
+echo '<h2> PHP info:  </h2>';
+
+phpinfo();
---
+++ examples/redis.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Welcome to Learn Lesson
+ * This is very Simple PHP Code of Caching
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
+ */
+
+use Phpfastcache\core\InstanceManager;
+
+// print_r(get_included_files());
+// print_r(get_loaded_extensions());
+
+// Include composer autoloader
+require 'vendor/autoload.php';
+
+$InstanceCache = InstanceManager::getInstance('redis');
+
+/**
+ * Try to get $products from Caching First
+ * product_page is "identity keyword";
+ */
+$key = "product_page";
+$CachedString = $InstanceCache->get($key);
+
+if (is_null($CachedString)) {
+    $CachedString = "Redis Cache --> Cache Enabled --> Well done !";
+    // Write products to Cache in 10 minutes with same keyword
+    $InstanceCache->set($key, $CachedString, 600);
+
+    echo "Redis Cache --> Cached not enabled --> Reload page !";
+
+} else {
+    echo $CachedString;
+}
+
+echo '<br /><br /><a href="/">Back to index</a>&nbsp;--&nbsp;<a href="/' . basename(__FILE__) . '">Reload</a>';

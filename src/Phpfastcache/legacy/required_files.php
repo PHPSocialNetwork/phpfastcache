339bc400575794ef9f4a35706cd2e4f166031679
Geolim4 <contact@geolim4.com>
2016-02-07 20:30:28 +0100

    Fixed relative path for non-composer fallback

--- src/Phpfastcache/legacy/required_files.php
+++ src/Phpfastcache/legacy/required_files.php
@@ -5,25 +5,25 @@
  */
 
 // Core files
-require_once '../core/DriverAbstract.php';
-require_once '../core/DriverInterface.php';
-require_once '../core/InstanceManager.php';
-require_once '../core/Phpfastcache.php';
-require_once '../core/PhpfastcacheExtensions.php';
+require_once __DIR__ . '/../core/DriverAbstract.php';
+require_once __DIR__ . '/../core/DriverInterface.php';
+require_once __DIR__ . '/../core/InstanceManager.php';
+require_once __DIR__ . '/../core/Phpfastcache.php';
+require_once __DIR__ . '/../core/PhpfastcacheExtensions.php';
 
 // Drivers files
-require_once '../drivers/apc.php';
-require_once '../drivers/cookie.php';
-require_once '../drivers/files.php';
-require_once '../drivers/memcache.php';
-require_once '../drivers/memcached.php';
-require_once '../drivers/predis.php';
-require_once '../drivers/redis.php';
-require_once '../drivers/sqlite.php';
-require_once '../drivers/ssdb.php';
-require_once '../drivers/wincache.php';
-require_once '../drivers/xcache.php';
+require_once __DIR__ . '/../drivers/apc.php';
+require_once __DIR__ . '/../drivers/cookie.php';
+require_once __DIR__ . '/../drivers/files.php';
+require_once __DIR__ . '/../drivers/memcache.php';
+require_once __DIR__ . '/../drivers/memcached.php';
+require_once __DIR__ . '/../drivers/predis.php';
+require_once __DIR__ . '/../drivers/redis.php';
+require_once __DIR__ . '/../drivers/sqlite.php';
+require_once __DIR__ . '/../drivers/ssdb.php';
+require_once __DIR__ . '/../drivers/wincache.php';
+require_once __DIR__ . '/../drivers/xcache.php';
 
 // Exceptions
-require_once '../exceptions/PhpfastcacheCoreException.php';
-require_once '../exceptions/PhpfastcacheDriverException.php';
+require_once __DIR__ . '/../exceptions/PhpfastcacheCoreException.php';
+require_once __DIR__ . '/../exceptions/PhpfastcacheDriverException.php';

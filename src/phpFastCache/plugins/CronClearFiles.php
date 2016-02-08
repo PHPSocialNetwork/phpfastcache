3b827160abc9ccc35ced8dc57538d3ecc204288c
Geolim4 <contact@geolim4.com>
2016-02-08 22:48:12 +0100

    Updated cron filename to be more psr consistent

--- src/phpFastCache/plugins/CronClearFiles.php
+++ src/phpFastCache/plugins/CronClearFiles.php
@@ -1,10 +1,12 @@
 <?php
+namespace phpFastCache\plugins;
+use phpFastCache\CacheManager;
 
-// Setup your cronjob to run this file every 30 mins - 60 mins to help clean up the expired files faster
+// Setup your cronjob to run this file every
+// 30 mins - 60 mins to help clean up
+// the expired files faster
 
 require_once (__DIR__ . "/../phpFastCache.php");
-
-use phpFastCache\CacheManager;
 
 $setup = array(
     "path"  => "/your_path/to_clean/"
@@ -13,4 +15,5 @@ $setup = array(
 $cache = CacheManager::Files($setup);
 
 // clean up expired files cache every hour
+// For now only "files" drivers is supported
 $cache->autoCleanExpired(3600*1);

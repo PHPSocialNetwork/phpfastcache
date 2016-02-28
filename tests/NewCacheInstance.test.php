7664cc6277ed92dbffa0c3927b460d3eb0a8857e
Geolim4 <contact@geolim4.com>
2016-02-28 13:19:21 +0100

    Added some (home made) unit tests

---
+++ tests/NewCacheInstance.test.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
+ * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ */
+use phpFastCache\CacheManager;
+use phpFastCache\Core\DriverInterface;
+
+chdir(__DIR__);
+require_once '../src/autoload.php';
+
+$status = 0;
+echo "Testing new cache instance\n";
+
+$driverInstance = CacheManager::getInstance();
+
+if(!is_object($driverInstance) || !($driverInstance instanceof DriverInterface))
+{
+    echo 'CacheManager::getInstance() returned wrong data:' . gettype($driverInstance) . "\n";
+    $status = 1;
+}
+echo "CacheManager::getInstance() returned expected object that implements DriverInterface\n";
+
+exit($status);

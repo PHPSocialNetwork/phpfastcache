cb594fab97d69e39214e6d6d4306e09cd9afe2be
Geolim4 <contact@geolim4.com>
2016-05-15 18:06:36 +0200

    Fixed Symfony profiler crashing

--- src/phpFastCache/Cache/DriverBaseTrait.php
+++ src/phpFastCache/Cache/DriverBaseTrait.php
@@ -330,4 +330,27 @@ trait DriverBaseTrait
     {
         return [];
     }
+
+    /**
+     * Serialization protection for frameworks
+     * with profiler such as Symfony, etc.
+     */
+    public function __sleep()
+    {
+        $this->itemInstances = array_keys($this->itemInstances);
+        return (array) array_keys(get_object_vars($this));
+    }
+
+    /**
+     * Serialization protection for frameworks
+     * with profiler such as Symfony, etc.
+     */
+    public function __wakeup()
+    {
+        $itemInstances = $this->itemInstances;
+        $this->itemInstances = [];
+        foreach ($itemInstances as $itemInstance) {
+            $this->itemInstances[$itemInstance] = $this->getItem($itemInstance);
+        }
+    }
 }

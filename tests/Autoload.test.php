4cc4c7747fa9e973c3739d35e96aa951ed88ea6d
Geolim4 <contact@geolim4.com>
2016-07-03 14:03:22 +0200

    Improved tests to test the CacheItemInterface

--- tests/Autoload.test.php
+++ tests/Autoload.test.php
@@ -12,11 +12,24 @@ require_once __DIR__ . '/../src/autoload.php';
 $status = 0;
 echo "Testing autoload\n";
 
+/**
+ * Testing PhpFastCache autoload
+ */
 if (!class_exists('phpFastCache\CacheManager')) {
     echo "[FAIL] Autoload failed to find the CacheManager\n";
     $status = 255;
 }else{
     echo "[PASS] Autoload successfully found the CacheManager\n";
+}
+
+/**
+ * Testing Psr autoload
+ */
+if (!interface_exists('Psr\Cache\CacheItemInterface')) {
+    echo "[FAIL] Autoload failed to find the Psr CacheItemInterface\n";
+    $status = 255;
+}else{
+    echo "[PASS] Autoload successfully found the Psr CacheItemInterface\n";
 }
 
 exit($status);

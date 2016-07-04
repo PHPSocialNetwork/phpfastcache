9bf7ea2f1e6bcc8e135c3c28814f06c27e0340dc
Geolim4 <contact@geolim4.com>
2016-07-05 00:52:43 +0200

    Starting V5.1 :-)

--- src/phpFastCache/Core/Item/ItemExtendedTrait.php
+++ src/phpFastCache/Core/Item/ItemExtendedTrait.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Geolim4
- * Date: 04/07/2016
- * Time: 23:29
+ *
+ * This file is part of phpFastCache.
+ *
+ * @license MIT License (MIT)
+ *
+ * For full copyright and license information, please see the docs/CREDITS.txt file.
+ *
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
+ * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ *
  */
 
 namespace phpFastCache\Core\Item;

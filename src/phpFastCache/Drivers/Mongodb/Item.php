a2f442bb47bd5216ea3d5a3b7e0c59859a28c7c4
Fabio Covolo Mazzo <fabio@vagascampinas.com.br>
2016-07-19 23:21:15 -0300

    Create MongoDb new Driver using MondoDb/Driver with mongo-php-library and rename MongoDb legacy driver to Mongo Driver.

--- src/phpFastCache/Drivers/Mongodb/Item.php
+++ src/phpFastCache/Drivers/Mongodb/Item.php
@@ -9,6 +9,7 @@
  *
  * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
  * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ * @author Fabio Covolo Mazzo (fabiocmazzo) <fabiomazzo@gmail.com>
  *
  */
 

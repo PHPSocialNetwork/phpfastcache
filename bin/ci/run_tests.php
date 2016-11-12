c7734dbb5a778f01f7892ffd4dd365b2f842519a
Geolim4 <contact@geolim4.com>
2016-11-12 01:38:03 +0100

    Added anti-regression test for #373

--- bin/ci/run_tests.php
+++ bin/ci/run_tests.php
@@ -4,12 +4,23 @@
  * @author Georges.L (Geolim4)  <contact@geolim4.com>
  */
 
+function glob_recursive($pattern, $flags = 0)
+{
+    $files = glob($pattern, $flags);
+
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+    {
+        $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+    }
+
+    return $files;
+}
+
 $status = 0;
 $driver = (!empty($argv[1]) ? ucfirst($argv[1]) : 'Files');
-
 $testDir = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tests');
 
-foreach (glob($testDir . DIRECTORY_SEPARATOR . '*.test.php') as $filename) {
+foreach (glob_recursive($testDir . DIRECTORY_SEPARATOR . '*.test.php') as $filename) {
     echo "\e[97m--\n";
     $command = "php -f {$filename} {$driver}";
     echo "\e[33mphpfastcache@test \e[34m~ \e[92m# \e[91m" . $command . "\n";

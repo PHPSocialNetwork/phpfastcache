c84d936a062d5a5c833cc9198450e8437c83828e
Geolim4 <contact@geolim4.com>
2017-01-25 21:19:30 +0100

    Removed deprecated ArrayAccess compatibility for driverStatistic entity

--- src/phpFastCache/Entities/driverStatistic.php
+++ src/phpFastCache/Entities/driverStatistic.php
@@ -13,15 +13,11 @@
  */
 namespace phpFastCache\Entities;
 
-use ArrayAccess;
-use InvalidArgumentException;
-use LogicException;
-
 /**
  * Class driverStatistic
  * @package phpFastCache\Entities
  */
-class driverStatistic implements ArrayAccess
+class driverStatistic
 {
     /**
      * @var string
@@ -131,97 +127,5 @@ class driverStatistic implements ArrayAccess
             'Data' => 'Cache items keys',
             'RawData' => 'Cache raw data',
         ];
-    }
-
-    /*****************
-     * ArrayAccess
-     *****************/
-
-    /**
-     * @param string $offset
-     * @param string $value
-     * @throws InvalidArgumentException
-     * @throws LogicException
-     */
-    public function offsetSet($offset, $value)
-    {
-        trigger_error($this->getDeprecatedMsg(), E_USER_DEPRECATED);
-        if (!is_string($offset)) {
-            throw new InvalidArgumentException('$offset must be a string');
-        } else {
-            if (property_exists($this, $offset)) {
-                $this->{$offset} = $value;
-            } else {
-                throw new LogicException("Property {$offset} does not exists");
-            }
-        }
-    }
-
-    /**
-     * @param string $offset
-     * @return bool
-     * @throws InvalidArgumentException
-     * @throws LogicException
-     */
-    public function offsetExists($offset)
-    {
-        trigger_error($this->getDeprecatedMsg(), E_USER_DEPRECATED);
-        if (!is_string($offset)) {
-            throw new InvalidArgumentException('$offset must be a string');
-        } else {
-            if (property_exists($this, $offset)) {
-                return isset($this->{$offset});
-            } else {
-                throw new LogicException("Property {$offset} does not exists");
-            }
-        }
-    }
-
-    /**
-     * @param string $offset
-     * @throws InvalidArgumentException
-     * @throws LogicException
-     */
-    public function offsetUnset($offset)
-    {
-        trigger_error($this->getDeprecatedMsg(), E_USER_DEPRECATED);
-        if (!is_string($offset)) {
-            throw new InvalidArgumentException('$offset must be a string');
-        } else {
-            if (property_exists($this, $offset)) {
-                unset($this->{$offset});
-            } else {
-                throw new LogicException("Property {$offset} does not exists");
-            }
-        }
-    }
-
-    /**
-     * @param string $offset
-     * @return string
-     * @throws InvalidArgumentException
-     * @throws LogicException
-     */
-    public function offsetGet($offset)
-    {
-        trigger_error($this->getDeprecatedMsg(), E_USER_DEPRECATED);
-        if (!is_string($offset)) {
-            throw new InvalidArgumentException('$offset must be a string');
-        } else {
-            if (property_exists($this, $offset)) {
-                return isset($this->{$offset}) ? $this->{$offset} : null;
-            } else {
-                throw new LogicException("Property {$offset} does not exists");
-            }
-        }
-    }
-
-    /**
-     * @return string
-     */
-    private function getDeprecatedMsg()
-    {
-        return 'You should consider upgrading your code and treat the statistic array as an object. 
-        The arrayAccess compatibility will be removed in the next major release (>=V6)';
     }
 }

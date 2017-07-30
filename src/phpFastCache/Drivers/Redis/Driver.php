d6cf085e6780cf8bae1dfeeec81f59b63e745af1
Geolim4 <contact@geolim4.com>
2017-07-30 23:31:15 +0200

    Fixed issue on (P)Redis with negative TTLs awaiting more specification of the PSR6 about this.

--- src/phpFastCache/Drivers/Redis/Driver.php
+++ src/phpFastCache/Drivers/Redis/Driver.php
@@ -70,7 +70,15 @@ class Driver implements ExtendedCacheItemPoolInterface
         if ($item instanceof Item) {
             $ttl = $item->getExpirationDate()->getTimestamp() - time();
 
-            return $this->instance->setex($item->getKey(), $ttl, $this->encode($this->driverPreWrap($item)));
+            /**
+             * @see https://redis.io/commands/setex
+             * @see https://redis.io/commands/expire
+             */
+            if($ttl <= 0){
+                return $this->instance->expire($item->getKey(), 0);
+            }else{
+                return $this->instance->setex($item->getKey(), $ttl, $this->encode($this->driverPreWrap($item)));
+            }
         } else {
             throw new phpFastCacheInvalidArgumentException('Cross-Driver type confusion detected');
         }

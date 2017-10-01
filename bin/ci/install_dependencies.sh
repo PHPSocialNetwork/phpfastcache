9fa97b69d96fad1f670234aa9ebd18396fe76f65
Geolim4 <contact@geolim4.com>
2017-10-01 22:09:16 +0200

    Mehhh, broken build again

--- bin/ci/install_dependencies.sh
+++ bin/ci/install_dependencies.sh
@@ -2,4 +2,4 @@
 
 composer self-update;
 composer install --ignore-platform-reqs;
-composer require "phpfastcache/couchdb:~1.0.0" "phpfastcache/phpssdb:~1.0.0" "predis/predis:~1.1.0" "mongodb/mongodb:^1.1" --ignore-platform-reqs;
+composer require "phpfastcache/couchdb:~1.0.0" "phpfastcache/phpssdb:~1.0.0" "predis/predis:~1.1.0" "mongodb/mongodb:^1.1" "basho/riak:^1.4" --ignore-platform-reqs;

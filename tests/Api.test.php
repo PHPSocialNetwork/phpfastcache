767289861d9d6784e5f7b1bedef3a33b93cc6c30
Geolim4 <contact@geolim4.com>
2017-10-24 00:58:37 +0200

    Opsyy, forgot the tests...

--- tests/Api.test.php
+++ tests/Api.test.php
@@ -6,14 +6,51 @@
  */
 
 use phpFastCache\Api;
-use phpFastCache\CacheManager;
-use phpFastCache\Exceptions\phpFastCacheDriverException;
+use phpFastCache\Exceptions\phpFastCacheRootException;
 use phpFastCache\Helper\TestHelper;
 
 chdir(__DIR__);
 require_once __DIR__ . '/../vendor/autoload.php';
 $testHelper = new TestHelper('API class');
 
-var_dump(Api::getPhpFastCacheVersion());
+/**
+ * Testing API version
+ */
+try {
+    $version = Api::getVersion();
+    $testHelper->printPassText(sprintf('Successfully retrieved the API version: %s', $version));
+} catch (phpFastCacheRootException $e) {
+    $testHelper->printFailText(sprintf('Failed to retrieve the API version with the following error error: %s', $e->getMessage()));
+}
+
+/**
+ * Testing PhpFastCache version
+ */
+try {
+    $version = Api::getPhpFastCacheVersion();
+    $testHelper->printPassText(sprintf('Successfully retrieved PhpFastCache version: %s', $version));
+} catch (phpFastCacheRootException $e) {
+    $testHelper->printFailText(sprintf('Failed to retrieve the PhpFastCache version with the following error error: %s', $e->getMessage()));
+}
+
+/**
+ * Testing API changelog
+ */
+try {
+    $changelog = Api::getChangelog();
+    $testHelper->printPassText(sprintf("Successfully retrieved API changelog:\n%s", $changelog));
+} catch (phpFastCacheRootException $e) {
+    $testHelper->printFailText(sprintf('Failed to retrieve the API changelog with the following error error: %s', $e->getMessage()));
+}
+
+/**
+ * Testing PhpFastCache changelog
+ */
+try {
+    $changelog = Api::getPhpFastCacheChangelog();
+    $testHelper->printPassText(sprintf("Successfully retrieved PhpFastCache changelog:\n%s", $changelog));
+} catch (phpFastCacheRootException $e) {
+    $testHelper->printFailText(sprintf('Failed to retrieve the PhpFastCache changelog with the following error error: %s', $e->getMessage()));
+}
 
 $testHelper->terminateTest();

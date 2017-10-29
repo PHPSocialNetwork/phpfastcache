cb1f7baf6b0c6523324f395f7a0939e71946f24d
Geolim4 <contact@geolim4.com>
2017-10-29 22:47:16 +0100

    Preparing website v2

--- ads.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-?>
-<script language="Javascript">
-    console.log("Ads Network is OK");
-</script>
--- testing.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-sleep("8");
-
-$data = array("a","b","c","d","e","f");
-echo json_encode($data);

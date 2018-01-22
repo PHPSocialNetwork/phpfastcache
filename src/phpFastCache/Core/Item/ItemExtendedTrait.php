PHPSocialNetwork/phpfastcache

commit d6f916002227e953c8a0609bd62ffb91f3cc6b42

Added Opcache improvements by namespacing php core's functions

src/phpFastCache/Core/Item/ItemExtendedTrait.php
@@ -64,7 +64,7 @@ trait ItemExtendedTrait
      */
     public function __construct(ExtendedCacheItemPoolInterface $driver, $key)
     {
-        if (is_string($key)) {
+        if (\is_string($key)) {
             $this->key = $key;
             $this->driver = $driver;
             $this->driver->setItem($this);
@@ -89,7 +89,7 @@ trait ItemExtendedTrait
             if ($keyHashFunction) {
                 $this->encodedKey = $keyHashFunction($this->getKey());
             } else {
-                $this->encodedKey = md5($this->getKey());
+                $this->encodedKey = \md5($this->getKey());
             }
         }
 
@@ -208,7 +208,7 @@ trait ItemExtendedTrait
      */
     public function increment($step = 1): ExtendedCacheItemInterface
     {
-        if (is_int($step)) {
+        if (\is_int($step)) {
             $this->fetched = true;
             $this->data += $step;
         } else {
@@ -225,7 +225,7 @@ trait ItemExtendedTrait
      */
     public function decrement($step = 1): ExtendedCacheItemInterface
     {
-        if (is_int($step)) {
+        if (\is_int($step)) {
             $this->fetched = true;
             $this->data -= $step;
         } else {
@@ -242,9 +242,9 @@ trait ItemExtendedTrait
      */
     public function append($data): ExtendedCacheItemInterface
     {
-        if (is_array($this->data)) {
+        if (\is_array($this->data)) {
             $this->data[] = $data;
-        } else if (is_string($data)) {
+        } else if (\is_string($data)) {
             $this->data .= (string)$data;
         } else {
             throw new phpFastCacheInvalidArgumentException('$data must be either array nor string.');
@@ -261,9 +261,9 @@ trait ItemExtendedTrait
      */
     public function prepend($data): ExtendedCacheItemInterface
     {
-        if (is_array($this->data)) {
-            array_unshift($this->data, $data);
-        } else if (is_string($data)) {
+        if (\is_array($this->data)) {
+            \array_unshift($this->data, $data);
+        } else if (\is_string($data)) {
             $this->data = (string)$data . $this->data;
         } else {
             throw new phpFastCacheInvalidArgumentException('$data must be either array nor string.');
@@ -279,8 +279,8 @@ trait ItemExtendedTrait
      */
     public function addTag($tagName): ExtendedCacheItemInterface
     {
-        if (is_string($tagName)) {
-            $this->tags = array_unique(array_merge($this->tags, [$tagName]));
+        if (\is_string($tagName)) {
+            $this->tags = \array_unique(\array_merge($this->tags, [$tagName]));
 
             return $this;
         } else {
@@ -308,8 +308,8 @@ trait ItemExtendedTrait
      */
     public function setTags(array $tags): ExtendedCacheItemInterface
     {
-        if (count($tags)) {
-            if (array_filter($tags, 'is_string')) {
+        if (\count($tags)) {
+            if (\array_filter($tags, 'is_string')) {
                 $this->tags = $tags;
             } else {
                 throw new phpFastCacheInvalidArgumentException('$tagName must be an array of string');
@@ -333,7 +333,7 @@ trait ItemExtendedTrait
      */
     public function getTagsAsString($separator = ', '): string
     {
-        return implode($separator, $this->tags);
+        return \implode($separator, $this->tags);
     }
 
     /**
@@ -368,27 +368,27 @@ trait ItemExtendedTrait
      */
     public function getRemovedTags(): array
     {
-        return array_diff($this->removedTags, $this->tags);
+        return \array_merge($this->removedTags, $this->tags);
     }
 
     /**
      * Return the data as a well-formatted string.
      * Any scalar value will be casted to an array
-     * @param int $option json_encode() options
-     * @param int $depth json_encode() depth
+     * @param int $option \json_encode() options
+     * @param int $depth \json_encode() depth
      * @return string
      */
     public function getDataAsJsonString($option = 0, $depth = 512): string
     {
         $data = $this->get();
 
-        if (is_object($data) || is_array($data)) {
-            $data = json_encode($data, $option, $depth);
+        if (\is_object($data) || \is_array($data)) {
+            $data = \json_encode($data, $option, $depth);
         } else {
-            $data = json_encode([$data], $option, $depth);
+            $data = \json_encode([$data], $option, $depth);
         }
 
-        return json_encode($data, $option, $depth);
+        return \json_encode($data, $option, $depth);
     }
 
     /**
@@ -421,8 +421,8 @@ trait ItemExtendedTrait
      */
     final public function __debugInfo()
     {
-        $info = get_object_vars($this);
-        $info[ 'driver' ] = 'object(' . get_class($info[ 'driver' ]) . ')';
+        $info = \get_object_vars($this);
+        $info[ 'driver' ] = 'object(' . \get_class($info[ 'driver' ]) . ')';
 
         return (array)$info;
     }

src/phpFastCache/Core/Pool/ExtendedCacheItemPoolTrait.php
@@ -35,7 +35,7 @@ trait ExtendedCacheItemPoolTrait
         $callback = function (CacheItemInterface $item) {
             return $item->get();
         };
-        return json_encode(array_map($callback, array_values($this->getItems($keys))), $option, $depth);
+        return \json_encode(array_map($callback, array_values($this->getItems($keys))), $option, $depth);
     }
 
     /**
@@ -43,7 +43,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function getItemsByTag($tagName)
     {
-        if (is_string($tagName)) {
+        if (\is_string($tagName)) {
             $driverResponse = $this->getItem($this->getTagKey($tagName));
             if ($driverResponse->isHit()) {
                 $items = (array)$driverResponse->get();
@@ -58,7 +58,7 @@ trait ExtendedCacheItemPoolTrait
                  *
                  * #headache
                  */
-                return array_filter($this->getItems(array_unique(array_keys($items))), function (ExtendedCacheItemInterface $item) {
+                return \array_filter($this->getItems(\array_unique(\array_keys($items))), function (ExtendedCacheItemInterface $item) {
                     return $item->isHit();
                 });
             } else {
@@ -75,9 +75,9 @@ trait ExtendedCacheItemPoolTrait
     public function getItemsByTags(array $tagNames)
     {
         $items = [];
-        foreach (array_unique($tagNames) as $tagName) {
-            if (is_string($tagName)) {
-                $items = array_merge($items, $this->getItemsByTag($tagName));
+        foreach (\array_unique($tagNames) as $tagName) {
+            if (\is_string($tagName)) {
+                $items = \array_merge($items, $this->getItemsByTag($tagName));
             } else {
                 throw new phpFastCacheInvalidArgumentException('$tagName must be a a string');
             }
@@ -95,7 +95,7 @@ trait ExtendedCacheItemPoolTrait
         $items = $this->getItemsByTags($tagNames);
 
         foreach ($items as $key => $item) {
-            if (array_diff($tagNames, $item->getTags())) {
+            if (\array_merge($tagNames, $item->getTags())) {
                 unset($items[ $key ]);
             }
         }
@@ -113,7 +113,7 @@ trait ExtendedCacheItemPoolTrait
             return $item->get();
         };
 
-        return json_encode(array_map($callback, array_values($this->getItemsByTags($tagNames))), $option, $depth);
+        return \json_encode(array_map($callback, array_values($this->getItemsByTags($tagNames))), $option, $depth);
     }
 
     /**
@@ -121,7 +121,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function deleteItemsByTag($tagName)
     {
-        if (is_string($tagName)) {
+        if (\is_string($tagName)) {
             $return = null;
             foreach ($this->getItemsByTag($tagName) as $item) {
                 $result = $this->deleteItem($item->getKey());
@@ -175,7 +175,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function incrementItemsByTag($tagName, $step = 1)
     {
-        if (is_string($tagName) && is_int($step)) {
+        if (\is_string($tagName) && \is_int($step)) {
             foreach ($this->getItemsByTag($tagName) as $item) {
                 $item->increment($step);
                 $this->saveDeferred($item);
@@ -208,7 +208,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function incrementItemsByTagsAll(array $tagNames, $step = 1)
     {
-        if (is_int($step)) {
+        if (\is_int($step)) {
             $items = $this->getItemsByTagsAll($tagNames);
 
             foreach ($items as $key => $item) {
@@ -226,7 +226,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function decrementItemsByTag($tagName, $step = 1)
     {
-        if (is_string($tagName) && is_int($step)) {
+        if (\is_string($tagName) && \is_int($step)) {
             foreach ($this->getItemsByTag($tagName) as $item) {
                 $item->decrement($step);
                 $this->saveDeferred($item);
@@ -259,7 +259,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function decrementItemsByTagsAll(array $tagNames, $step = 1)
     {
-        if (is_int($step)) {
+        if (\is_int($step)) {
             $items = $this->getItemsByTagsAll($tagNames);
 
             foreach ($items as $key => $item) {
@@ -277,7 +277,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function appendItemsByTag($tagName, $data)
     {
-        if (is_string($tagName)) {
+        if (\is_string($tagName)) {
             foreach ($this->getItemsByTag($tagName) as $item) {
                 $item->append($data);
                 $this->saveDeferred($item);
@@ -328,7 +328,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function prependItemsByTag($tagName, $data)
     {
-        if (is_string($tagName)) {
+        if (\is_string($tagName)) {
             foreach ($this->getItemsByTag($tagName) as $item) {
                 $item->prepend($data);
                 $this->saveDeferred($item);
@@ -400,7 +400,7 @@ trait ExtendedCacheItemPoolTrait
      */
     public function attachItem(CacheItemInterface $item)
     {
-        if (isset($this->itemInstances[ $item->getKey() ]) && spl_object_hash($item) !== spl_object_hash($this->itemInstances[ $item->getKey() ])) {
+        if (isset($this->itemInstances[ $item->getKey() ]) && \spl_object_hash($item) !== \spl_object_hash($this->itemInstances[ $item->getKey() ])) {
             throw new phpFastCacheLogicException('The item already exists and cannot be overwritten because the Spl object hash mismatches ! You probably tried to re-attach a detached item which has been already retrieved from cache.');
         } else {
             $this->itemInstances[ $item->getKey() ] = $item;
@@ -418,7 +418,7 @@ trait ExtendedCacheItemPoolTrait
         if ($item instanceof CacheItemInterface) {
             unset($this->itemInstances[ $item->getKey() ]);
 
-        } else if (is_string($item)) {
+        } else if (\is_string($item)) {
             unset($this->itemInstances[ $item ]);
         } else {
             throw new phpFastCacheInvalidArgumentException('Invalid type for $item variable');
@@ -447,7 +447,7 @@ trait ExtendedCacheItemPoolTrait
     public function isAttached(CacheItemInterface $item)
     {
         if (isset($this->itemInstances[ $item->getKey() ])) {
-            return spl_object_hash($item) === spl_object_hash($this->itemInstances[ $item->getKey() ]);
+            return \spl_object_hash($item) === \spl_object_hash($this->itemInstances[ $item->getKey() ]);
         }
         return null;
     }
@@ -470,12 +470,12 @@ trait ExtendedCacheItemPoolTrait
      */
     public function saveMultiple(...$items): bool
     {
-        if (isset($items[ 0 ]) && is_array($items[ 0 ])) {
+        if (isset($items[ 0 ]) && \is_array($items[ 0 ])) {
             foreach ($items[ 0 ] as $item) {
                 $this->save($item);
             }
             return true;
-        } else if (is_array($items)) {
+        } else if (\is_array($items)) {
             foreach ($items as $item) {
                 $this->save($item);
             }

src/phpFastCache/Drivers/Sqlite/Driver.php
@@ -76,7 +76,7 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     public function driverCheck(): bool
     {
-        return extension_loaded('pdo_sqlite') && (is_writable($this->getSqliteDir()) || @mkdir($this->getSqliteDir(), $this->getDefaultChmod(), true));
+        return extension_loaded('pdo_sqlite') && (\is_writable($this->getSqliteDir()) || @mkdir($this->getSqliteDir(), $this->getDefaultChmod(), true));
     }
 
     /**
@@ -85,10 +85,10 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     protected function driverConnect(): bool
     {
-        if (!file_exists($this->getSqliteDir()) && !@mkdir($this->getSqliteDir(), $this->getDefaultChmod(), true)) {
+        if (!\file_exists($this->getSqliteDir()) && !@mkdir($this->getSqliteDir(), $this->getDefaultChmod(), true)) {
             throw new phpFastCacheIOException(sprintf('Sqlite cannot write in "%s", aborting...', $this->getPath()));
         }
-        if (!file_exists($this->getPath() . '/' . self::FILE_DIR)) {
+        if (!\file_exists($this->getPath() . '/' . self::FILE_DIR)) {
             if (!mkdir($this->getPath() . '/' . self::FILE_DIR, $this->getDefaultChmod(), true)
             ) {
                 $this->fallback = true;
@@ -282,7 +282,7 @@ class Driver implements ExtendedCacheItemPoolInterface
     {
         if ($this->indexing == null) {
             $createTable = false;
-            if (!file_exists($this->SqliteDir . '/indexing')) {
+            if (!\file_exists($this->SqliteDir . '/indexing')) {
                 $createTable = true;
             }
 
@@ -309,7 +309,7 @@ class Driver implements ExtendedCacheItemPoolInterface
 
             // check file size
 
-            $size = file_exists($this->SqliteDir . '/db' . $db) ? filesize($this->SqliteDir . '/db' . $db) : 1;
+            $size = \file_exists($this->SqliteDir . '/db' . $db) ? filesize($this->SqliteDir . '/db' . $db) : 1;
             $size = round($size / 1024 / 1024, 1);
 
 
@@ -361,7 +361,7 @@ class Driver implements ExtendedCacheItemPoolInterface
         if (!isset($this->instance[ $instant ])) {
             // check DB Files ready or not
             $createTable = false;
-            if (!file_exists($this->SqliteDir . '/db' . $instant) || $reset == true) {
+            if (!\file_exists($this->SqliteDir . '/db' . $instant) || $reset == true) {
                 $createTable = true;
             }
             $PDO = new PDO('sqlite:' . $this->SqliteDir . '/db' . $instant);
@@ -384,6 +384,6 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     public function __sleep(): array
     {
-        return array_diff(array_keys(get_object_vars($this)), ['indexing', 'instance']);
+        return \array_merge(\array_keys(\get_object_vars($this)), ['indexing', 'instance']);
     }
 }

src/phpFastCache/Drivers/Apc/Driver.php
@@ -123,7 +123,7 @@ class Driver implements ExtendedCacheItemPoolInterface
         $date = (new \DateTime())->setTimestamp($stats[ 'start_time' ]);
 
         return (new DriverStatistic())
-          ->setData(implode(', ', array_keys($this->itemInstances)))
+          ->setData(\implode(', ', \array_keys($this->itemInstances)))
           ->setInfo(sprintf("The APC cache is up since %s, and have %d item(s) in cache.\n For more information see RawData.", $date->format(DATE_RFC2822),
             $stats[ 'num_entries' ]))
           ->setRawData($stats)

src/phpFastCache/Drivers/Apcu/Driver.php
@@ -119,7 +119,7 @@ class Driver implements ExtendedCacheItemPoolInterface
         $date = (new \DateTime())->setTimestamp($stats[ 'start_time' ]);
 
         return (new DriverStatistic())
-          ->setData(implode(', ', array_keys($this->itemInstances)))
+          ->setData(\implode(', ', \array_keys($this->itemInstances)))
           ->setInfo(sprintf("The APCU cache is up since %s, and have %d item(s) in cache.\n For more information see RawData.", $date->format(DATE_RFC2822),
             $stats[ 'num_entries' ]))
           ->setRawData($stats)

src/phpFastCache/Drivers/Cassandra/Driver.php
@@ -278,7 +278,7 @@ HELP;
         return (new DriverStatistic())
           ->setSize($result->first()[ 'cache_size' ])
           ->setRawData([])
-          ->setData(implode(', ', array_keys($this->itemInstances)))
+          ->setData(\implode(', ', \array_keys($this->itemInstances)))
           ->setInfo('The cache size represents only the cache data itself without counting data structures associated to the cache entries.');
     }
 

src/phpFastCache/Drivers/Cookie/Driver.php
@@ -37,7 +37,7 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     public function driverCheck(): bool
     {
-        if (function_exists('setcookie')) {
+        if (\function_exists('setcookie')) {
             return true;
         } else {
             return false;
@@ -49,7 +49,7 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     protected function driverConnect(): bool
     {
-        return !(!array_key_exists('phpFastCache', $_COOKIE) && !@setcookie('phpFastCache', 1, 10));
+        return !(!\array_key_exists('phpFastCache', $_COOKIE) && !@setcookie('phpFastCache', 1, 10));
     }
 
     /**
@@ -61,7 +61,7 @@ class Driver implements ExtendedCacheItemPoolInterface
     {
         $this->driverConnect();
         $keyword = self::PREFIX . $item->getKey();
-        $x = isset($_COOKIE[ $keyword ]) ? json_decode($_COOKIE[ $keyword ], true) : false;
+        $x = isset($_COOKIE[ $keyword ]) ? \json_decode($_COOKIE[ $keyword ], true) : false;
 
         if ($x == false) {
             return null;
@@ -87,7 +87,7 @@ class Driver implements ExtendedCacheItemPoolInterface
         if ($item instanceof Item) {
             $this->driverConnect();
             $keyword = self::PREFIX . $item->getKey();
-            $v = json_encode($this->driverPreWrap($item));
+            $v = \json_encode($this->driverPreWrap($item));
 
             if (isset($this->config[ 'limited_memory_each_object' ]) && strlen($v) > $this->config[ 'limited_memory_each_object' ]) {
                 return false;
@@ -107,7 +107,7 @@ class Driver implements ExtendedCacheItemPoolInterface
     {
         $this->driverConnect();
         $keyword = self::PREFIX . $key;
-        $x = isset($_COOKIE[ $keyword ]) ? $this->decode(json_decode($_COOKIE[ $keyword ])->t) : 0;
+        $x = isset($_COOKIE[ $keyword ]) ? $this->decode(\json_decode($_COOKIE[ $keyword ])->t) : 0;
 
         return $x ? $x - time() : $x;
     }
@@ -141,7 +141,7 @@ class Driver implements ExtendedCacheItemPoolInterface
         $return = null;
         $this->driverConnect();
         foreach ($_COOKIE as $keyword => $value) {
-            if (strpos($keyword, self::PREFIX) !== false) {
+            if (\strpos($keyword, self::PREFIX) !== false) {
                 $_COOKIE[ $keyword ] = null;
                 $result = @setcookie($keyword, null, -10);
                 if ($return !== false) {
@@ -172,7 +172,7 @@ class Driver implements ExtendedCacheItemPoolInterface
          * Only count PFC Cookie
          */
         foreach ($_COOKIE as $key => $value) {
-            if (strpos($key, self::PREFIX) === 0) {
+            if (\strpos($key, self::PREFIX) === 0) {
                 $size += strlen($value);
             }
         }

src/phpFastCache/Drivers/Couchbase/Driver.php
@@ -164,7 +164,7 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     protected function setBucket($bucketName, \CouchbaseBucket $CouchbaseBucket)
     {
-        if (!array_key_exists($bucketName, $this->bucketInstances)) {
+        if (!\array_key_exists($bucketName, $this->bucketInstances)) {
             $this->bucketInstances[ $bucketName ] = $CouchbaseBucket;
         } else {
             throw new phpFastCacheLogicException('A bucket instance with this name already exists.');
@@ -187,7 +187,7 @@ class Driver implements ExtendedCacheItemPoolInterface
         return (new DriverStatistic())
           ->setSize($info[ 'basicStats' ][ 'diskUsed' ])
           ->setRawData($info)
-          ->setData(implode(', ', array_keys($this->itemInstances)))
+          ->setData(\implode(', ', \array_keys($this->itemInstances)))
           ->setInfo('CouchBase version ' . $info[ 'nodes' ][ 0 ][ 'version' ] . ', Uptime (in days): ' . round($info[ 'nodes' ][ 0 ][ 'uptime' ] / 86400,
               1) . "\n For more information see RawData.");
     }

commit 816e6f3fc4b1b35badc470e49f54526d14f6812c

Fixed regression effect introduced by d6f916002227e953c8a0609bd62ffb91f3cc6b42

src/phpFastCache/Core/Item/ItemExtendedTrait.php
@@ -368,7 +368,7 @@ trait ItemExtendedTrait
      */
     public function getRemovedTags(): array
     {
-        return \array_merge($this->removedTags, $this->tags);
+        return \array_diff($this->removedTags, $this->tags);
     }
 
     /**

src/phpFastCache/Core/Pool/ExtendedCacheItemPoolTrait.php
@@ -95,7 +95,7 @@ trait ExtendedCacheItemPoolTrait
         $items = $this->getItemsByTags($tagNames);
 
         foreach ($items as $key => $item) {
-            if (\array_merge($tagNames, $item->getTags())) {
+            if (\array_diff($tagNames, $item->getTags())) {
                 unset($items[ $key ]);
             }
         }

src/phpFastCache/Drivers/Sqlite/Driver.php
@@ -384,6 +384,6 @@ class Driver implements ExtendedCacheItemPoolInterface
      */
     public function __sleep(): array
     {
-        return \array_merge(\array_keys(\get_object_vars($this)), ['indexing', 'instance']);
+        return \array_diff(\array_keys(\get_object_vars($this)), ['indexing', 'instance']);
     }
 }
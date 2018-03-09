6da60d8a23c88e38ffbdec50066d3457b072e7f5
Geolim4 <contact@geolim4.com>
2018-03-09 22:33:46 +0100

    Fixed deprecated tests setup

--- tests/issues/Github-467.test.php
+++ tests/issues/Github-467.test.php
@@ -16,21 +16,21 @@ $testHelper = new TestHelper('Github issue #467 - Allow to specify the file exte
 CacheManager::setDefaultConfig(new ConfigurationOption(['path' => __DIR__ . '/../../cache']));
 
 try{
-    $cacheInstance = CacheManager::getInstance('Files', ['cacheFileExtension' => 'php']);
+    $cacheInstance = CacheManager::getInstance('Files', new ConfigurationOption(['cacheFileExtension' => 'php']));
     $testHelper->printFailText('No error thrown while trying to setup a dangerous file extension');
 }catch(phpFastCacheInvalidConfigurationException $e){
     $testHelper->printPassText('An error has been thrown while trying to setup a dangerous file extension');
 }
 
 try{
-    $cacheInstance = CacheManager::getInstance('Files', ['cacheFileExtension' => '.cache']);
+    $cacheInstance = CacheManager::getInstance('Files', new ConfigurationOption(['cacheFileExtension' => '.cache']));
     $testHelper->printFailText('No error thrown while trying to setup a dotted file extension');
 }catch(phpFastCacheInvalidConfigurationException $e){
     $testHelper->printPassText('An error has been thrown while trying to setup a dotted file extension');
 }
 
 try{
-    $cacheInstance = CacheManager::getInstance('Files', ['cacheFileExtension' => 'cache']);
+    $cacheInstance = CacheManager::getInstance('Files', new ConfigurationOption(['cacheFileExtension' => 'cache']));
     $testHelper->printPassText('No error thrown while trying to setup a safe file extension');
 }catch(phpFastCacheInvalidConfigurationException $e){
     $testHelper->printFailText('An error has been thrown while trying to setup a safe file extension');

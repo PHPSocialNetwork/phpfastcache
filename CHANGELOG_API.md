311aed1cb9deaf92e269f00c10d4835a287f81c0
Geolim4 <contact@geolim4.com>
2018-03-09 23:28:28 +0100

    Moved API changelog to a standalone file

---
+++ CHANGELOG_API.md
@@ -0,0 +1,69 @@
+## 2.0.0
+- Introduced BC breaks:
+- Updated ExtendedCacheItemPoolInterface to be compliant with the new \$config object introduced in V7.
+- ExtendedCacheItemPoolInterface::getConfig() no longer returns an array but a ConfigurationOption object
+- ExtendedCacheItemPoolInterface::getDefaultConfig() no longer returns an array but a ConfigurationOption object
+
+## 1.3.0
+- Implemented full PHP7 type hint support for ExtendedCacheItemPoolInterface and ExtendedCacheItemInterface
+- Added instance ID getter (introduced in V7):
+  - ExtendedCacheItemPoolInterface::getInstanceId()
+- The method ExtendedCacheItemPoolInterface::getDefaultConfig() will now returns a \phpFastCache\Util\ArrayObject
+
+## 1.2.5
+- Implemented additional simple helper method to direct access to a config option:
+  - ExtendedCacheItemPoolInterface::getConfigOption()
+
+## 1.2.4
+- Implemented additional simple helper method to provide basic information about the driver:
+  - ExtendedCacheItemPoolInterface::getHelp()
+
+## 1.2.3
+- Implemented additional saving method form multiple items:
+   ExtendedCacheItemPoolInterface::saveMultiple()
+
+## 1.2.2
+- Implemented additional tags methods such as:
+  - ExtendedCacheItemPoolInterface::getItemsByTagsAll()
+  - ExtendedCacheItemPoolInterface::incrementItemsByTagsAll()
+  - ExtendedCacheItemPoolInterface::decrementItemsByTagsAll()
+  - ExtendedCacheItemPoolInterface::deleteItemsByTagsAll()
+  - ExtendedCacheItemPoolInterface::appendItemsByTagsAll()
+  - ExtendedCacheItemPoolInterface::prependItemsByTagsAll()
+
+## 1.2.1
+- Implemented Event manager methods such as:
+  - ExtendedCacheItemInterface::setEventManager()
+  - ExtendedCacheItemPoolInterface::setEventManager()
+
+## 1.2.0
+- Implemented Item advanced time methods such as:
+  - ExtendedCacheItemInterface::setExpirationDate() (Alias of CacheItemInterface::ExpireAt() for more code logic)
+  - ExtendedCacheItemInterface::getCreationDate() * 
+  - ExtendedCacheItemInterface::getModificationDate() *
+  - ExtendedCacheItemInterface::setCreationDate(\DateTimeInterface) *
+  - ExtendedCacheItemInterface::setModificationDate() *
+    - \* Require configuration directive "itemDetailedDate" to be enabled
+
+## 1.1.3
+- Added an additional CacheItemInterface method:
+  - ExtendedCacheItemInterface::getEncodedKey()
+
+## 1.1.2
+- Implemented [de|a]ttaching methods to improve memory management
+  - ExtendedCacheItemPoolInterface::detachItem()
+  - ExtendedCacheItemPoolInterface::detachAllItems()
+  - ExtendedCacheItemPoolInterface::attachItem()
+  - ExtendedCacheItemPoolInterface::isAttached()
+
+## 1.1.1
+- Implemented JsonSerializable interface to ExtendedCacheItemInterface
+
+## 1.1.0
+- Implemented JSON methods such as:
+  - ExtendedCacheItemPoolInterface::getItemsAsJsonString()
+  - ExtendedCacheItemPoolInterface::getItemsByTagsAsJsonString()
+  - ExtendedCacheItemInterface::getDataAsJsonString()
+
+## 1.0.0
+- First initial version
--- lib/Phpfastcache/Api.php
+++ lib/Phpfastcache/Api.php
@@ -100,7 +100,7 @@ class Api
      * @param bool $cacheable
      * @return string
      */
-    public static function getPhpFastCacheGitHeadHash($cacheable = true)
+    public static function getPhpFastCacheGitHeadHash($cacheable = true): string
     {
         static $hash;
 
@@ -120,83 +120,22 @@ class Api
 
 
     /**
-     * @todo Put this content to a file !!!!
      * Return the API changelog, as a string.
      * @return string
+     * @throws phpFastCacheLogicException
+     * @throws phpFastCacheIOException
      */
     public static function getChangelog(): string
     {
-        return <<<CHANGELOG
-- 2.0.0
--- Introduced BC breaks:
--- Updated ExtendedCacheItemPoolInterface to be compliant with the new \$config object introduced in V7.
--- ExtendedCacheItemPoolInterface::getConfig() no longer returns an array but a ConfigurationOption object
--- ExtendedCacheItemPoolInterface::getDefaultConfig() no longer returns an array but a ConfigurationOption object
-
-- 1.3.0
--- Implemented full PHP7 type hint support for ExtendedCacheItemPoolInterface and ExtendedCacheItemInterface
--- Added instance ID getter (introduced in V7):
-   ExtendedCacheItemPoolInterface::getInstanceId()
--- The method ExtendedCacheItemPoolInterface::getDefaultConfig() will now return a \phpFastCache\Util\ArrayObject
-
-- 1.2.5
--- Implemented additional simple helper method to direct access to a config option:
-   ExtendedCacheItemPoolInterface::getConfigOption()
-
-- 1.2.4
--- Implemented additional simple helper method to provide basic information about the driver:
-   ExtendedCacheItemPoolInterface::getHelp()
-
-- 1.2.3
--- Implemented additional saving method form multiple items:
-   ExtendedCacheItemPoolInterface::saveMultiple()
-
-- 1.2.2
--- Implemented additional tags methods such as:
-   ExtendedCacheItemPoolInterface::getItemsByTagsAll()
-   ExtendedCacheItemPoolInterface::incrementItemsByTagsAll()
-   ExtendedCacheItemPoolInterface::decrementItemsByTagsAll()
-   ExtendedCacheItemPoolInterface::deleteItemsByTagsAll()
-   ExtendedCacheItemPoolInterface::appendItemsByTagsAll()
-   ExtendedCacheItemPoolInterface::prependItemsByTagsAll()
-
-- 1.2.1
--- Implemented Event manager methods such as:
-   ExtendedCacheItemInterface::setEventManager()
-   ExtendedCacheItemPoolInterface::setEventManager()
-
-- 1.2.0
--- Implemented Item advanced time methods such as:
-   ExtendedCacheItemInterface::setExpirationDate() (Alias of CacheItemInterface::ExpireAt() for more code logic)
-   ExtendedCacheItemInterface::getCreationDate() * 
-   ExtendedCacheItemInterface::getModificationDate() *
-   ExtendedCacheItemInterface::setCreationDate(\DateTimeInterface) *
-   ExtendedCacheItemInterface::setModificationDate() *
-   * Require configuration directive "itemDetailedDate" to be enabled
-
-- 1.1.3
--- Added an additional CacheItemInterface method:
-   ExtendedCacheItemInterface::getEncodedKey()
-
-- 1.1.2
--- Implemented [de|a]ttaching methods to improve memory management
-   ExtendedCacheItemPoolInterface::detachItem()
-   ExtendedCacheItemPoolInterface::detachAllItems()
-   ExtendedCacheItemPoolInterface::attachItem()
-   ExtendedCacheItemPoolInterface::isAttached()
-
-- 1.1.1
--- Implemented JsonSerializable interface to ExtendedCacheItemInterface
-
-- 1.1.0
--- Implemented JSON methods such as:
-   ExtendedCacheItemPoolInterface::getItemsAsJsonString()
-   ExtendedCacheItemPoolInterface::getItemsByTagsAsJsonString()
-   ExtendedCacheItemInterface::getDataAsJsonString()
-
-- 1.0.0
--- First initial version
-CHANGELOG;
+        $changelogFilename = __DIR__ . '/../../CHANGELOG_API.md';
+        if(\file_exists($changelogFilename)){
+            $string = \str_replace(["\r\n", "\r"], "\n", \trim(file_get_contents($changelogFilename)));
+            if($string){
+                return $string;
+            }
+            throw new phpFastCacheLogicException('Unable to retrieve the PhpFastCache API changelog as it seems to be empty.');
+        }
+        throw new phpFastCacheIOException('The CHANGELOG_API.md file is not readable or has been removed.');
     }
 
     /**

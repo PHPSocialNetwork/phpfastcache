c1ee506eeae5be7324711b04b7e19913d5f901b6
Geolim4 <contact@geolim4.com>
2018-05-23 22:01:03 +0200

    Fixed RedisCustomClient test behavior if the Redis extension is missing

--- tests/RedisCustomClient.test.php
+++ tests/RedisCustomClient.test.php
@@ -6,6 +6,7 @@
  */
 
 use Phpfastcache\CacheManager;
+use Phpfastcache\Exceptions\PhpfastcacheDriverCheckException;
 use Phpfastcache\Helper\TestHelper;
 use Phpfastcache\Drivers\Redis\Config as RedisConfig;
 use Redis as RedisClient;
@@ -15,6 +16,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $testHelper = new TestHelper('Redis custom client');
 
 try{
+    if(!class_exists(RedisClient::class)){
+        throw new PhpfastcacheDriverCheckException('Unable to test Redis client because the extension seems to be missing');
+    }
     $redisClient = new RedisClient();
     $redisClient->connect('127.0.0.1', 6379, 5);
     $redisClient->select(0);

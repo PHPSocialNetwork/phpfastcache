8e6ae28ccc8bc6fa596f6b254c781f0de20b9167
Geolim4 <contact@geolim4.com>
2018-08-17 21:46:17 +0200

    Fixed wrong copyright annotation in some file headersq

--- lib/Phpfastcache/Drivers/Leveldb/Config.php
+++ lib/Phpfastcache/Drivers/Leveldb/Config.php
@@ -1,10 +1,18 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Geolim4
- * Date: 12/02/2018
- * Time: 23:10
+ *
+ * This file is part of phpFastCache.
+ *
+ * @license MIT License (MIT)
+ *
+ * For full copyright and license information, please see the docs/CREDITS.txt file.
+ *
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> https://www.phpfastcache.com
+ * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ *
  */
+
+declare(strict_types=1);
 
 namespace Phpfastcache\Drivers\Leveldb;
 
@@ -18,18 +26,18 @@ class Config extends ConfigurationOption
     protected $htaccess = true;
 
     /**
-     * @return bool
+     * @return string
      */
-    public function getHtaccess(): bool
+    public function getHtaccess(): string
     {
         return $this->htaccess;
     }
 
     /**
-     * @param bool $htaccess
+     * @param string $htaccess
      * @return self
      */
-    public function setHtaccess(bool $htaccess): self
+    public function setHtaccess(string $htaccess): self
     {
         $this->htaccess = $htaccess;
         return $this;

e890ae68497d18e43d0232762b87ade7f3e97ceb
Geolim4 <contact@geolim4.com>
2018-09-08 20:35:11 +0200

    Moved exclusive files-related configurations keys to IOConfigurationTrait

--- lib/Phpfastcache/Config/IOConfigurationOptionTrait.php
+++ lib/Phpfastcache/Config/IOConfigurationOptionTrait.php
@@ -14,6 +14,10 @@
 declare(strict_types=1);
 namespace Phpfastcache\Config;
 
+use Phpfastcache\Exceptions\PhpfastcacheInvalidConfigurationException;
+
+const SAFE_FILE_EXTENSIONS = 'txt|cache|db|pfc';
+
 trait IOConfigurationOptionTrait
 {
     /**
@@ -32,6 +36,11 @@ trait IOConfigurationOptionTrait
     protected $securityKey = '';
 
     /**
+     * @var string
+     */
+    protected $cacheFileExtension = 'txt';
+
+    /**
      * @return string
      */
     public function getSecurityKey(): string
@@ -43,7 +52,7 @@ trait IOConfigurationOptionTrait
      * @param string $securityKey
      * @return Config
      */
-    public function setSecurityKey(string $securityKey): ConfigurationOptionInterface
+    public function setSecurityKey(string $securityKey): self
     {
         $this->securityKey = $securityKey;
 
@@ -81,9 +90,44 @@ trait IOConfigurationOptionTrait
      * @param bool $secureFileManipulation
      * @return self
      */
-    public function setSecureFileManipulation(bool $secureFileManipulation): ConfigurationOptionInterface
+    public function setSecureFileManipulation(bool $secureFileManipulation): self
     {
         $this->secureFileManipulation = $secureFileManipulation;
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getCacheFileExtension(): string
+    {
+        return $this->cacheFileExtension;
+    }
+
+    /**
+     * @param string $cacheFileExtension
+     * @return self
+     * @throws PhpfastcacheInvalidConfigurationException
+     */
+    public function setCacheFileExtension(string $cacheFileExtension): self
+    {
+        /**
+         * Feel free to propose your own one
+         * by opening a pull request :)
+         */
+        $safeFileExtensions = \explode('|', SAFE_FILE_EXTENSIONS);
+
+        if (\strpos($cacheFileExtension, '.') !== false) {
+            throw new PhpfastcacheInvalidConfigurationException('cacheFileExtension cannot contain a dot "."');
+        }
+        if (!\in_array($cacheFileExtension, $safeFileExtensions, true)) {
+            throw new PhpfastcacheInvalidConfigurationException(
+                "Extension \"{$cacheFileExtension}\" is not safe, currently allowed extension names: " . \implode(', ', $safeFileExtensions)
+            );
+        }
+
+        $this->cacheFileExtension = $cacheFileExtension;
         return $this;
     }
 }
--- tests/issues/Github-467.test.php
+++ tests/issues/Github-467.test.php
@@ -6,31 +6,31 @@
  */
 
 use Phpfastcache\CacheManager;
-use Phpfastcache\Config\ConfigurationOption;
+use Phpfastcache\Drivers\Files\Config as FilesConfig;
 use Phpfastcache\Exceptions\PhpfastcacheInvalidConfigurationException;
 use Phpfastcache\Helper\TestHelper;
 
 chdir(__DIR__);
 require_once __DIR__ . '/../../vendor/autoload.php';
 $testHelper = new TestHelper('Github issue #467 - Allow to specify the file extension in the File Driver');
-CacheManager::setDefaultConfig(new ConfigurationOption(['path' => __DIR__ . '/../../cache']));
+CacheManager::setDefaultConfig(new FilesConfig(['path' => __DIR__ . '/../../cache']));
 
 try{
-    $cacheInstance = CacheManager::getInstance('Files', new ConfigurationOption(['cacheFileExtension' => 'php']));
+    $cacheInstance = CacheManager::getInstance('Files', new FilesConfig(['cacheFileExtension' => 'php']));
     $testHelper->printFailText('No error thrown while trying to setup a dangerous file extension');
 }catch(PhpfastcacheInvalidConfigurationException $e){
     $testHelper->printPassText('An error has been thrown while trying to setup a dangerous file extension');
 }
 
 try{
-    $cacheInstance = CacheManager::getInstance('Files', new ConfigurationOption(['cacheFileExtension' => '.cache']));
+    $cacheInstance = CacheManager::getInstance('Files', new FilesConfig(['cacheFileExtension' => '.cache']));
     $testHelper->printFailText('No error thrown while trying to setup a dotted file extension');
 }catch(PhpfastcacheInvalidConfigurationException $e){
     $testHelper->printPassText('An error has been thrown while trying to setup a dotted file extension');
 }
 
 try{
-    $cacheInstance = CacheManager::getInstance('Files', new ConfigurationOption(['cacheFileExtension' => 'cache']));
+    $cacheInstance = CacheManager::getInstance('Files', new FilesConfig(['cacheFileExtension' => 'cache']));
     $testHelper->printPassText('No error thrown while trying to setup a safe file extension');
 }catch(PhpfastcacheInvalidConfigurationException $e){
     $testHelper->printFailText('An error has been thrown while trying to setup a safe file extension');

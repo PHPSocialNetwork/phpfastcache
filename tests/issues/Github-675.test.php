866ef9b06d1e0f184d41bf932fd94100a75bafe4
Geolim4 <contact@geolim4.com>
2019-03-03 14:45:13 +0100

    Fixed #675

---
+++ tests/issues/Github-675.test.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> https://www.phpfastcache.com
+ * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ */
+
+use Phpfastcache\CacheManager;
+use Phpfastcache\Drivers\Memcached\Config;
+use Phpfastcache\Exceptions\PhpfastcacheDriverException;
+use Phpfastcache\Exceptions\PhpfastcacheInvalidConfigurationException;
+use Phpfastcache\Helper\TestHelper;
+
+chdir(__DIR__);
+require_once __DIR__ . '/../../vendor/autoload.php';
+$testHelper = new TestHelper('Github issue #675 - Memcached ignores custom host/port configurations');
+
+try {
+    $config = new Config();
+    $config->setServers([
+        [
+            'invalidHostKey' => '120.0.13.37',
+            'invalidPortKey' => 8080,
+            'saslUser' => false,
+            'saslPassword' => false,
+        ]
+    ]);
+    $testHelper->printFailText('1/4 Memcached config accepted unknown key(s) from $servers array.');
+} catch (PhpfastcacheInvalidConfigurationException $e) {
+    $testHelper->printPassText('1/4 Memcached config detected unknown key(s) from $servers array: ' . $e->getMessage());
+}
+
+try {
+    $config = new Config();
+    $config->setServers([
+        [
+            'host' => '120.0.13.37',
+            'unwantedKey' => '120.0.13.37',
+            'unwantedKey2' => '120.0.13.37',
+            'port' => 8080,
+            'saslUser' => false,
+            'saslPassword' => false,
+        ]
+    ]);
+    $testHelper->printFailText('2/4 Memcached config accepted unwanted key(s) from $servers array.');
+} catch (PhpfastcacheInvalidConfigurationException $e) {
+    $testHelper->printPassText('2/4 Memcached config detected unwanted key(s) from $servers array: ' . $e->getMessage());
+}
+
+try {
+    $config = new Config();
+    $config->setServers([
+        [
+            'host' => '120.0.13.37',
+            'port' => '8080',
+            'saslUser' => false,
+            'saslPassword' => false,
+        ]
+    ]);
+    $testHelper->printFailText('3/4 Memcached config does not detected invalid types fort host and port');
+} catch (PhpfastcacheInvalidConfigurationException $e) {
+    $testHelper->printPassText('3/4 Memcached config detected invalid types fort host and port: ' . $e->getMessage());
+}
+
+try {
+    $config = new Config();
+    $config->setHost('255.255.255.255');
+    $config->setPort(1337);
+    $cacheInstance = CacheManager::getInstance('Memcached', $config);
+    $testHelper->printFailText('4/4 Memcached succeeded to connect with invalid host/port specified (used default combination of "$config->servers")');
+} catch (PhpfastcacheDriverException $e) {
+    $testHelper->printPassText('4/4 Memcached failed to connect with invalid host/port specified');
+}
+
+$testHelper->terminateTest();

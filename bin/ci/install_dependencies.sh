c94fd9020ad438db68f18f3c7cffeadadf264aec
Geolim4 <contact@geolim4.com>
2021-08-15 04:48:01 +0200

    Fixed pecl Mongodb install

--- bin/ci/install_dependencies.sh
+++ bin/ci/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 composer self-update;
-composer install --ignore-platform-reqs;
-composer require "phpfastcache/couchdb:~1.0.0" "phpfastcache/phpssdb:~1.0.0" "predis/predis:~1.1.0" "mongodb/mongodb:^1.1" "phpfastcache/riak-client:^1.4.4" --ignore-platform-reqs;
+composer install;
+composer require "phpfastcache/couchdb:~1.0.0" "phpfastcache/phpssdb:~1.0.0" "predis/predis:~1.1.0" "mongodb/mongodb:^1.1" "phpfastcache/riak-client:^1.4.4";

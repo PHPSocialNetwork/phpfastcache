702fcfbed3eb9c19dc88a16fe543bf1c9878fd90
Geolim4 <contact@geolim4.com>
2021-09-04 21:53:43 +0200

    Updated phpcs test

--- tests/000-quality/phpcs.test.php
+++ tests/000-quality/phpcs.test.php
@@ -18,21 +18,21 @@ use Phpfastcache\Tests\Helper\TestHelper;
 
 chdir(__DIR__);
 require_once __DIR__ . '/../../vendor/autoload.php';
-$testHelper = new TestHelper('Quality: PHPMD');
+$testHelper = new TestHelper('Quality: PHPCS');
 
 //
 chdir(__DIR__ . '/../../');
 
-$binary = realpath(getcwd() . '/vendor/bin/phpmd');
+$binary = realpath(getcwd() . '/vendor/bin/phpcs');
 
-exec($binary . ' lib/ ansi phpmd.xml', $output, $resultCode);
+exec($binary . ' lib/  --report=summary', $output, $resultCode);
 
 if($resultCode === 0){
-    $testHelper->assertPass('Great, PHPMD found no violation on the project');
+    $testHelper->assertPass('Great, PHPCS found no errors on the project');
 }else{
-    $testHelper->assertFail('Oh no, PHPMD found some violations on the project, full report available below:');
+    $testHelper->assertFail('Oh no, PHPCS found some errors on the project, full report available below:');
     $testHelper->printText(str_repeat('#',  100));
-    $testHelper->printText(array_map(static fn ($str) => $str ? '    ' . $str : '', array_slice($output, 1)));
+    $testHelper->printText(array_map(static fn ($str) => $str ? '    ' . $str : '', $output));
     $testHelper->printText(str_repeat('#',  100));
 }
 

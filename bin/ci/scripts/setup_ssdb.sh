287beb88bae2b940fd14ce850e2716399353bd08
Geolim4 <contact@geolim4.com>
2021-09-05 04:43:04 +0200

    Updated SSDB script

--- bin/ci/scripts/setup_ssdb.sh
+++ bin/ci/scripts/setup_ssdb.sh
@@ -5,5 +5,11 @@ set -e
 wget --no-check-certificate https://github.com/ideawu/ssdb/archive/master.zip
 unzip -q master
 cd ssdb-master
-make
-./ssdb-server -d ssdb.conf
+
+make > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "[OK] Make command succeeded, running server..."
+    ./ssdb-server -d ssdb.conf
+else
+    echo "[KO] Make command failed, the server will NOT be running"
+fi

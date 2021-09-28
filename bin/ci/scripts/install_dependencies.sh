b7d76a11e2997092a4a2f19c153c7c728921713b
Geolim4 <contact@geolim4.com>
2021-09-28 20:55:41 +0200

    Fixed tests

--- bin/ci/scripts/install_dependencies.sh
+++ bin/ci/scripts/install_dependencies.sh
@@ -3,4 +3,4 @@
 composer self-update
 composer validate
 composer install
-composer require doctrine/couchdb:dev-master phpfastcache/phpssdb:~1.1.0 predis/predis:~1.1.0 mongodb/mongodb:^1.9 triagens/arangodb:^3.8 aws/aws-sdk-php:~3.0 google/cloud-firestore:^1.20
+composer require -W doctrine/couchdb:dev-master phpfastcache/phpssdb:~1.1.0 predis/predis:~1.1.0 mongodb/mongodb:^1.9 triagens/arangodb:^3.8 aws/aws-sdk-php:~3.0 google/cloud-firestore:^1.20

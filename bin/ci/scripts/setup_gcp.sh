38e588edac4b9fd054ac4c19133b0059056a35c6
Geolim4 <contact@geolim4.com>
2021-10-31 02:26:15 +0100

    Fixed bug with unavailable secret variable for PR

--- bin/ci/scripts/setup_gcp.sh
+++ bin/ci/scripts/setup_gcp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-printenv BASE64_GOOGLE_APPLICATION_CREDENTIALS | base64 --decode > "${GOOGLE_APPLICATION_CREDENTIALS}"
+if [ -z ${BASE64_GOOGLE_APPLICATION_CREDENTIALS+x} ] || [ -z ${GOOGLE_APPLICATION_CREDENTIALS+x} ];
+then
+    echo "GCP secret variables are not set, ignoring..."
+else
+    printenv BASE64_GOOGLE_APPLICATION_CREDENTIALS | base64 --decode > "${GOOGLE_APPLICATION_CREDENTIALS}"
+fi

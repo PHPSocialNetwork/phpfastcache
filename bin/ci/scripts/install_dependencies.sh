ad52a7443e22964564c4c172d267461df7a5fff0
Geolim4 <contact@geolim4.com>
2022-03-27 15:26:53 +0200

    Lowered Mongodb minimum version that fails with Travis

--- bin/ci/scripts/install_dependencies.sh
+++ bin/ci/scripts/install_dependencies.sh
@@ -3,4 +3,7 @@
 composer self-update
 composer validate
 composer install
-composer require -W doctrine/couchdb:dev-master phpfastcache/phpssdb:~1.1 predis/predis:~1.1 mongodb/mongodb:~1.10 triagens/arangodb:~3.8 aws/aws-sdk-php:~3.2 google/cloud-firestore:~1.20 solarium/solarium:~6.1
+#####
+# Travis CI have php mongodb extension locked to 1.10, so we must set the mongodb/mongodb minimum version to 1.9 :(
+#####
+composer require -W doctrine/couchdb:dev-master phpfastcache/phpssdb:~1.1 predis/predis:~1.1 mongodb/mongodb:~1.9 triagens/arangodb:~3.8 aws/aws-sdk-php:~3.2 google/cloud-firestore:~1.20 solarium/solarium:~6.1
